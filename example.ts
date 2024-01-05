56a04a0d4b8aceaf3fcafc3206ec3281a5554ed7
Garrett Serack <gserack@gmail.com>
2024-01-05 09:23:16 -0800

    good

--- example.ts
+++ example.ts
@@ -1,3 +1,7 @@
 // example deno script
-// can be run with deno run -A https://...
+// can be run with `deno run -A https://raw.githubusercontent.com/fearthecowboy/scripts/main/example.ts`
+import { readFile, readdir } from 'node:fs/promises';
+
 console.log('hi')
+const results = await readdir(".");
+console.log(results);
